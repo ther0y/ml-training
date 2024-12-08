aad7431e10aa14fbb95d79d208b3226f06cbe0c0
Masood Rezazadeh <MasoodRezazadeh@gmail.com>
2024-12-08 22:14:33 +0330

    Initial commit

---
+++ src/dataset.py
@@ -0,0 +1,7 @@
+# import pandas as pd
+
+# data = {"Name": ["Alice", "Bob"], "Age": [24, 27], "City": ["New york", "Tehran"]}
+
+# df = pd.DataFrame(data)
+
+# df.replace("Alice", "Jane Doe").replace("Bob", "John Doe")
